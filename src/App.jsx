098cf057c0394f52d706a231f8da599292edaae4
GOLF <theerawat14886@gmail.com>
2023-12-07 13:19:58 +0700

    addbuttonlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [addlist, setAddlist] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const onClickAdd = () => {
+    setAddlist([...addlist, inputValue]);
+    setInputValue("");
+  };
+  const list = addlist.map((item, index) => (
+    <div key={index} className={"list-item"}>
+      {item}
+    </div>
+  ));
+  return (
+    <>
+      <div className="container">
+        <h1 className="head">Welcome TaskList</h1>
+        <div className="list">{list}</div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className="add-button" onClick={onClickAdd}>
+          add
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
